02022f41f02e3e9356fff4b54e3c0193ba470daa
Michael Kim <mkim0407@gmail.com>
2016-03-06 15:18:45 +0800

    Line endings: logging

--- py/line-endings.py
+++ py/line-endings.py
@@ -8,6 +8,8 @@ TO = "\n"
 INCLUDE = []
 EXCLUDE = []
 
+logger = logging.getLogger("line-endings")
+
 def has_any(name, l):
     for item in l:
         if item in name:
@@ -23,8 +25,8 @@ def recursive(func):
             if os.path.isfile(path):
                 if INCLUDE and not has_any(path, INCLUDE):
                     continue
-                logging.debug("Processing file: " + path)
-                func(path, *args, **kwargs)
+                count = func(path, *args, **kwargs)
+                logger.info("In {:<64}: {:>8} lines converted".format(path, count))
             else:
                 new_func(path, *args, **kwargs)
     return new_func
@@ -33,9 +35,12 @@ def recursive(func):
 def convert(filename):
     with open(filename) as f:
         text = f.read()
-    text = text.replace(FROM, TO)
-    with open(filename, "w") as f:
-        f.write(text)
+    count = text.count(FROM)
+    if count > 0:
+        text = text.replace(FROM, TO)
+        with open(filename, "w") as f:
+            f.write(text)
+    return count
 
 def print_help():
     print """line-endings.py [options] [includes] [! excludes]
@@ -56,6 +61,11 @@ if __name__ == "__main__":
     from sys import argv
     args = argv[1:]
 
+    logging.basicConfig(
+            level=logging.INFO,
+            format="[%(levelname)s] %(message)s"
+            )
+
     # Options
     while args and args[0].startswith("-"):
         if args[0] in ["-h", "--help"]:
@@ -63,12 +73,14 @@ if __name__ == "__main__":
             exit(0)
         elif args[0] in ["-r", "--revert"]:
             FROM, TO = TO, FROM
+            logger.info("Option specified: Revert from \\n to \\r\\n")
         args = args[1:]
 
     # Includes
     while args and args[0] != "!":
         INCLUDE.append(args[0])
         args = args[1:]
+    logger.info("Includes: {}".format(INCLUDE))
 
     # Excludes
     if args and args[0] == "!":
@@ -76,6 +88,7 @@ if __name__ == "__main__":
     while args:
         EXCLUDE.append(args[0])
         args = args[1:]
+    logger.info("Excludes: {}".format(EXCLUDE))
 
     convert(".")
 
