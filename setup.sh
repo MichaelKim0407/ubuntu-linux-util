9215de7839f817a8d1643bcdab2d9415ff81ae62
Michael Kim <mkim0407@gmail.com>
2017-04-02 08:55:20 +0800

    setup & update

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-cp -r ./bash ~/bash
-cp -r ./py ~/py
+mkdir ~/bash
+cp ./bash/* ~/bash/
+
+mkdir ~/py
+cp -r ./py/* ~/py/
+
 cp ./vimrc ~/.vimrc
+
+cp ./ps1 ~/.bash_ps1
+echo "Remember to add to the end of ~/.bashrc:"
+echo "if [ -f ~/.bash_ps1 ]; then"
+echo "    . ~/.bash_ps1"
+echo "fi"
 
--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 rm -rf ./bash && cp -r ~/bash ./bash
 rm -rf ./py && cp -r ~/py ./py
 cp ~/.vimrc ./vimrc
+cp ~/.bash_ps1 ./ps1
 
