61937f87d3bdc35a1843fe1fbd592e8d3a4df14c
Michael Kim <mkim0407@gmail.com>
2017-06-05 05:02:47 -0700

    pip-upgrade-all.py: output to stdout and stderr

--- py/pip-upgrade-all.py
+++ py/pip-upgrade-all.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python
 
-from subprocess import check_output
+import sys
+from subprocess import check_call, check_output, STDOUT, CalledProcessError
 
 PIP_MIN_VERSION = 9
-pips = ['pip2', 'pip3']
 
-for pip in pips:
-    print("Using {}".format(pip))
+def pip_upgrade_all(pip):
+    print("--- Using {} ---".format(pip))
     out = check_output([pip, "--version"])
     version = out.split()[1]
     major = int(version.split(".")[0])
     if major < PIP_MIN_VERSION:
         print("{} version is {}; at least {} is required".format(
             pip, version, PIP_MIN_VERSION))
-        continue
+        return
+
     out = check_output([pip, "list", "--outdated"])
     packages = []
     for line in out.split("\n")[2:]:
@@ -24,11 +25,20 @@ for pip in pips:
         packages.append(name)
     print("{} packages need to be upgraded".format(len(packages)))
     if not packages:
-        continue
+        return
+
     print("They are: {}".format(packages))
     try:
-        out = check_output([pip, "install", "-U"] + packages)
-    except:
+        print("Upgrading all packages...")
+        check_call([pip, "install", "-U"] + packages, stdout=sys.stdout, stderr=sys.stderr)
+    except CalledProcessError:
         print("Upgrade failed. Please upgrade manually, or fix the problem.")
     else:
         print("Upgrade successful.")
+
+if __name__ == "__main__":
+    for pip in ['pip2', 'pip3']:
+        try:
+            pip_upgrade_all(pip)
+        except Exception as e:
+            print(e)
