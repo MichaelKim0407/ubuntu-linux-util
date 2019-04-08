4e13b74058b944ef7a523541941f55c30b14706a
Michael Kim <mkim0407@gmail.com>
2019-04-07 23:31:04 -0700

    fix bash script style

--- settings/minimize.sh
+++ settings/minimize.sh
@@ -2,9 +2,9 @@
 
 opt="true"
 
-if [ $1 ]
+if [[ $1 ]]
 then
     opt="$1"
 fi
 
-gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window $opt
+gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window ${opt}
